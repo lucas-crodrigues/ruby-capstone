3b932eba4ff7aa15fdad562778283a57d39e14f5
nimplay <nimrod7day@gmail.com>
2022-10-10 17:39:35 -0400

    Test for game class

--- classes/game.rb
+++ classes/game.rb
@@ -2,12 +2,13 @@ require 'date'
 require_relative './item'
 
 class Game < Item
-  attr_accessor :last_played, :multiplayer
+  attr_accessor :last_played, :multiplayer, :publish_date
   attr_reader :id
 
-  def initialize(last_played, multiplayer)
+  def initialize(last_played, multiplayer, publish_date)
     super(id = Random.rand(1..1000))
     @id = id
+    @publish_date = publish_date
     @last_played = last_played
     @multiplayer = multiplayer
     @archived = can_be_archived?
