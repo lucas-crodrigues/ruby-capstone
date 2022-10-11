40c74b2520cbfd3bb540a30baae16e21399a468f
lucascr <lcrodrigues1995@gmail.com>
2022-10-11 12:06:03 -0300

    Create main.rb

---
+++ classes/main.rb
@@ -0,0 +1,10 @@
+require_relative './app'
+
+def main
+  puts '***-----------------------------------------------------------***'
+  puts '        Welcome to catalog of my things'
+  puts "***-----------------------------------------------------------***\n"
+  app = App.new
+end
+
+main
