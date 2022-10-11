3ba44aafad6b1d222087ec301c81e36c3205fe7f
jaferIdrees <jaferll@gmail.com>
2022-10-11 19:16:09 +0300

    add label class

---
+++ classes/label.rb
@@ -0,0 +1,20 @@
+class Label
+  def initialize(title, color)
+    @id = rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+
+  def as_hash
+    {
+      'title' => @title,
+      'color' => @color
+    }
+  end
+end
