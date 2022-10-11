35f63d343c9dbfdd3a7fd99ed91018531d4d1c57
nimplay <nimrod7day@gmail.com>
2022-10-11 15:24:38 -0400

    Adapting code to main code

--- classes/author.rb
+++ classes/author.rb
@@ -1,20 +1,16 @@
-require 'date'
-require_relative './item'
-
-class Author < Item
-  attr_accessor :first_name, :last_name, :items
+class Author
+  attr_accessor :first_name, :last_name
   attr_reader :id
 
   def initialize(first_name, last_name)
-    super(id = Random.rand(1..1000))
-    @id = id
+    @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
   def add_item(item)
-    (item.is_a?(Item) && @items.include?(item)) || (@items << item)
-    item.add_author(self)
+    @items << item unless @item.include?(item)
+    item.author = self
   end
 end
