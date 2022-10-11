ded66b9165312c242dbb305e1c5043abdf227977
jaferIdrees <jaferll@gmail.com>
2022-10-11 16:55:19 +0300

    Add accessors for publisher and cver_state

--- classes/book.rb
+++ classes/book.rb
@@ -1,6 +1,7 @@
 require_relative './item.rb'
 
 class Book << Item
+  attr_accessor :publisher, :cover_state
   def initialize(publish_date,publisher, cover_state)
     super(publish_date)
     @publisher = publisher
